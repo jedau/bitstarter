81be6a60edd7dbaa7c731db5edd246fef439e552
Jed Aureus Gonzales <gonzales.au@gmail.com>
2013-07-08 05:47:47 +0000

    Fixed web.js error for Part 1 [6]

--- web.js
+++ web.js
@@ -3,8 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fs = require('fs'); 
-  response.send(fs.readFile('index.html', 'utf8'));
+  var fs = require('fs');
+  var buf = new Buffer(256); 
+  response.send(buf.toString(fs.readFile('index.html', 'utf8')));
 });
 
 var port = process.env.PORT || 5000;
